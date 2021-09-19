5ff647621a4ce249ecfbdd5ce7aec939b7d1c1f8
DefaultMC <69502660+DefaultMC@users.noreply.github.com>
2021-09-19 01:47:47 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+let menu = document.querySelector(".menu-responsive");
+let navbar = document.querySelector(".navbar");
+let header = document.querySelector("header");
+
+
+menu.addEventListener("click", ()=>{
+    header.classList.toggle("active");
+    navbar.classList.toggle("show");
+    
+} )
+
+window.onscroll = () =>{
+    header.classList.remove("active");
+    navbar.classList.remove("show");
+
+}
+
+function loader(){
+    document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut(){
+    setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
+
+
+var swiper = new Swiper(".oferts-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+
+    },
+});
